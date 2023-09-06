1ff9ccb835113ac53462171d3d9a9b4da1d32f75
‘na-yujin’ <90164093+nayujina@users.noreply.github.com>
2023-09-07 02:01:17 +0900

    Feat : 최적화02 - React.memo 고착컴포넌트 만들어보기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import {useMemo, useEffect, useRef, useState} from "react";
+import OptimizeTest from "./OptimizeTest";
 // import Lifecycle from "./Lifecycle";
 
 // https://jsonplaceholder.typicode.com/comments
@@ -47,7 +48,7 @@ function App() {
   };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`)
+    // console.log(`${targetId}가 삭제되었습니다.`)
     const newDiaryList = data.filter((it) => it.id !== targetId)
     setData(newDiaryList)
   };
@@ -62,7 +63,7 @@ function App() {
 
   const getDiaryAnalysis = useMemo(
     () => {
-    console.log("일기 분석 시작")
+    // console.log("일기 분석 시작")
 
     const goodCount = data.filter((it) => it.emotion >=3).length
     const badCount = data.length - goodCount
@@ -74,6 +75,7 @@ function App() {
 
   return (
     <div className="App">
+      <OptimizeTest />
       {/*<Lifecycle />*/}
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
